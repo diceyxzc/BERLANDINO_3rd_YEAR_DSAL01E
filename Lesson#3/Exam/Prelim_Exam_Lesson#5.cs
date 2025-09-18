cb3c153728247cf80e89f4245734ace680818f30
Calvin Berlandino <2023-2-02178@lpunetwork.edu.ph>
2025-09-18 09:54:54 +0800

    aaa

--- Lesson#3/Exam/Prelim_Exam_Lesson#5.cs
+++ Lesson#3/Exam/Prelim_Exam_Lesson#5.cs
@@ -47,6 +47,7 @@ namespace Lesson_3.Exam
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            // disable textboxes that are not needed
             sss_contrib_txtbox.Enabled = false;
             philhealth_contrib_txtbox.Enabled = false;
             pagibig_contrib_txtbox.Enabled = false;
@@ -62,24 +63,27 @@ namespace Lesson_3.Exam
         private void gross_income_btn_Click(object sender, EventArgs e)
         {
             // convert into numeric variables
+            // basic rate
             basic_rate = Convert.ToDouble(basic_rate_txtbox.Text);
             basic_numhrs = Double.Parse(basic_numhrs_txtbox.Text);
             basic_netincome = basic_numhrs * basic_rate;
             basic_net_income_txtbox.Text = basic_netincome.ToString("n");
 
+            // hono rate
             hono_numhrs = Convert.ToDouble(hono_numhrs_txtbox.Text);
             hono_rate = Convert.ToDouble(hono_rate_txtbox.Text);
             hono_netincome = hono_numhrs * hono_rate;
             hono_net_income_txtbox.Text = hono_netincome.ToString("n");
 
+            // other rate
             other_numhrs = Convert.ToDouble(other_numhrs_txtbox.Text);
             other_rate = Convert.ToDouble(other_rate_txtbox.Text);
             other_netincome = other_numhrs * other_rate;
             other_net_income_txtbox.Text = other_netincome.ToString("n");
 
+            // compute gross income & pagibig
             gross_income = basic_netincome + hono_netincome + other_netincome;
-            gross_income_txtbox.Text = gross_income.ToString("n");
-
+            gross_income_txtbox.Text = "₱ " + gross_income.ToString("n");
             pagibig_contrib_txtbox.Text = pagibig_contrib.ToString("n");
 
             // tax contrib
@@ -89,26 +93,25 @@ namespace Lesson_3.Exam
             }
             else if (gross_income > 10416.67 && gross_income <= 16666.67)
             {
-                tax_contrib = (((gross_income * 24) - 250000) * 0.20) / 24;
+                tax_contrib = (((gross_income * 24) - 250000) * 0.15) / 24;
             }
             else if (gross_income > 16666.67 && gross_income <= 33333.33)
             {
-                tax_contrib = ((((gross_income * 24) - 400000) * 0.25) + 30000) / 24;
+                tax_contrib = ((((gross_income * 24) - 400000) * 0.20) + 30000) / 24;
             }
             else if (gross_income > 33333.33 && gross_income <= 83333.33)
             {
-                tax_contrib = ((((gross_income * 24) - 800000) * 0.30) + 130000) / 24;
+                tax_contrib = ((((gross_income * 24) - 800000) * 0.25) + 130000) / 24;
             }
             else if (gross_income > 83333.33 && gross_income <= 333333.33)
             {
-                tax_contrib = ((((gross_income * 24) - 2000000) * 0.32) + 490000) / 24;
+                tax_contrib = ((((gross_income * 24) - 2000000) * 0.30) + 490000) / 24;
             }
             else
             {
                 tax_contrib = ((((gross_income * 24) - 8000000) * 0.35) + 2410000) / 24;
             }
             tax_contrib_txtbox.Text = tax_contrib.ToString("N");
-
 
             // philhealth contrib
             if (gross_income <= 10000)
@@ -128,247 +131,247 @@ namespace Lesson_3.Exam
             // sss contrib
             if (gross_income < 5250)
             {
-                sss_contrib = 760.00;
+                sss_contrib = 250.00;
             }
             else if (gross_income >= 5250 && gross_income < 5750)
             {
-                sss_contrib = 835.00;
+                sss_contrib = 275.00;
             }
             else if (gross_income >= 5750 && gross_income < 6250)
             {
-                sss_contrib = 910.00;
+                sss_contrib = 300.00;
             }
             else if (gross_income >= 6250 && gross_income < 6750)
             {
-                sss_contrib = 985.00;
+                sss_contrib = 325.00;
             }
             else if (gross_income >= 6750 && gross_income < 7250)
             {
-                sss_contrib = 1060.00;
+                sss_contrib = 350.00;
             }
             else if (gross_income >= 7250 && gross_income < 7750)
             {
-                sss_contrib = 1135.00;
+                sss_contrib = 375.00;
             }
             else if (gross_income >= 7750 && gross_income < 8250)
             {
-                sss_contrib = 1210.00;
+                sss_contrib = 400.00;
             }
             else if (gross_income >= 8250 && gross_income < 8750)
             {
-                sss_contrib = 1285.00;
+                sss_contrib = 425.00;
             }
             else if (gross_income >= 8750 && gross_income < 9250)
             {
-                sss_contrib = 1360.00;
+                sss_contrib = 450.00;
             }
             else if (gross_income >= 9250 && gross_income < 9750)
             {
-                sss_contrib = 1435.00;
+                sss_contrib = 475.00;
             }
             else if (gross_income >= 9750 && gross_income < 10250)
             {
-                sss_contrib = 1510.00;
+                sss_contrib = 500.00;
             }
             else if (gross_income >= 10250 && gross_income < 10750)
             {
-                sss_contrib = 1585.00;
+                sss_contrib = 525.00;
             }
             else if (gross_income >= 10750 && gross_income < 11250)
             {
-                sss_contrib = 1660.00;
+                sss_contrib = 550.00;
             }
             else if (gross_income >= 11250 && gross_income < 11750)
             {
-                sss_contrib = 1735.00;
+                sss_contrib = 575.00;
             }
             else if (gross_income >= 11750 && gross_income < 12250)
             {
-                sss_contrib = 1810.00;
+                sss_contrib = 600.00;
             }
             else if (gross_income >= 12250 && gross_income < 12750)
             {
-                sss_contrib = 1885.00;
+                sss_contrib = 625.00;
             }
             else if (gross_income >= 12750 && gross_income < 13250)
             {
-                sss_contrib = 1960.00;
+                sss_contrib = 650.00;
             }
             else if (gross_income >= 13250 && gross_income < 13750)
             {
-                sss_contrib = 2035.00;
+                sss_contrib = 675.00;
             }
             else if (gross_income >= 13750 && gross_income < 14250)
             {
-                sss_contrib = 2110.00;
+                sss_contrib = 700.00;
             }
             else if (gross_income >= 14250 && gross_income < 14750)
             {
-                sss_contrib = 2185.00;
+                sss_contrib = 725.00;
             }
             else if (gross_income >= 14750 && gross_income < 15250)
             {
-                sss_contrib = 2280.00;
+                sss_contrib = 750.00;
             }
             else if (gross_income >= 15250 && gross_income < 15750)
             {
-                sss_contrib = 2355.00;
+                sss_contrib = 775.00;
             }
             else if (gross_income >= 15750 && gross_income < 16250)
             {
-                sss_contrib = 2430.00;
+                sss_contrib = 800.00;
             }
             else if (gross_income >= 16250 && gross_income < 16750)
             {
-                sss_contrib = 2505.00;
+                sss_contrib = 825.00;
             }
             else if (gross_income >= 16750 && gross_income < 17250)
             {
-                sss_contrib = 2580.00;
+                sss_contrib = 850.00;
             }
             else if (gross_income >= 17250 && gross_income < 17750)
             {
-                sss_contrib = 2655.00;
+                sss_contrib = 875.00;
             }
             else if (gross_income >= 17750 && gross_income < 18250)
             {
-                sss_contrib = 2730.00;
+                sss_contrib = 900.00;
             }
             else if (gross_income >= 18250 && gross_income < 18750)
             {
-                sss_contrib = 2785.00;
+                sss_contrib = 925.00;
             }
             else if (gross_income >= 18750 && gross_income < 19250)
             {
-                sss_contrib = 2880.00;
+                sss_contrib = 950.00;
             }
             else if (gross_income >= 19250 && gross_income < 19750)
             {
-                sss_contrib = 2955.00;
+                sss_contrib = 975.00;
             }
             else if (gross_income >= 19750 && gross_income < 20250)
             {
-                sss_contrib = 3030.00;
+                sss_contrib = 1000.00;
             }
             else if (gross_income >= 20250 && gross_income < 20750)
             {
-                sss_contrib = 3105.00;
+                sss_contrib = 1025.00;
             }
             else if (gross_income >= 20750 && gross_income < 21250)
             {
-                sss_contrib = 3180.00;
+                sss_contrib = 1050.00;
             }
             else if (gross_income >= 21250 && gross_income < 21750)
             {
-                sss_contrib = 3255.00;
+                sss_contrib = 1075.00;
             }
             else if (gross_income >= 21750 && gross_income < 22250)
             {
-                sss_contrib = 3330.00;
+                sss_contrib = 1100.00;
             }
             else if (gross_income >= 22250 && gross_income < 22750)
             {
-                sss_contrib = 3405.00;
+                sss_contrib = 1125.00;
             }
             else if (gross_income >= 22750 && gross_income < 23250)
             {
-                sss_contrib = 3480.00;
+                sss_contrib = 1150.00;
             }
             else if (gross_income >= 23250 && gross_income < 23750)
             {
-                sss_contrib = 3555.00;
+                sss_contrib = 1175.00;
             }
             else if (gross_income >= 23750 && gross_income < 24250)
             {
-                sss_contrib = 3630.00;
+                sss_contrib = 1200.00;
             }
             else if (gross_income >= 24250 && gross_income < 24750)
             {
-                sss_contrib = 3705.00;
+                sss_contrib = 1225.00;
             }
             else if (gross_income >= 24750 && gross_income < 25250)
             {
-                sss_contrib = 3780.00;
+                sss_contrib = 1250.00;
             }
             else if (gross_income >= 25250 && gross_income < 25750)
             {
-                sss_contrib = 3855.00;
+                sss_contrib = 1275.00;
             }
             else if (gross_income >= 25750 && gross_income < 26250)
             {
-                sss_contrib = 3930.00;
+                sss_contrib = 1300.00;
             }
             else if (gross_income >= 26250 && gross_income < 26750)
             {
-                sss_contrib = 4005.00;
+                sss_contrib = 1325.00;
             }
             else if (gross_income >= 26750 && gross_income < 27250)
             {
-                sss_contrib = 4080.00;
+                sss_contrib = 1350.00;
             }
             else if (gross_income >= 27250 && gross_income < 27750)
             {
-                sss_contrib = 4155.00;
+                sss_contrib = 1375.00;
             }
             else if (gross_income >= 27750 && gross_income < 28250)
             {
-                sss_contrib = 4230.00;
+                sss_contrib = 1400.00;
             }
             else if (gross_income >= 28250 && gross_income < 28750)
             {
-                sss_contrib = 4305.00;
+                sss_contrib = 1425.00;
             }
             else if (gross_income >= 28750 && gross_income < 29250)
             {
-                sss_contrib = 4380.00;
+                sss_contrib = 1450.00;
             }
             else if (gross_income >= 29250 && gross_income < 29750)
             {
-                sss_contrib = 4455.00;
+                sss_contrib = 1475.00;
             }
             else if (gross_income >= 29750 && gross_income < 30250)
             {
-                sss_contrib = 4530.00;
+                sss_contrib = 1500.00;
             }
             else if (gross_income >= 30250 && gross_income < 30750)
             {
-                sss_contrib = 4605.00;
+                sss_contrib = 1525.00;
             }
             else if (gross_income >= 30750 && gross_income < 31250)
             {
-                sss_contrib = 4680.00;
+                sss_contrib = 1550.00;
             }
             else if (gross_income >= 31250 && gross_income < 31750)
             {
-                sss_contrib = 4755.00;
+                sss_contrib = 1575.00;
             }
             else if (gross_income >= 31750 && gross_income < 32250)
             {
-                sss_contrib = 4830.00;
+                sss_contrib = 1600.00;
             }
             else if (gross_income >= 32250 && gross_income < 32750)
             {
-                sss_contrib = 4905.00;
+                sss_contrib = 1625.00;
             }
             else if (gross_income >= 32750 && gross_income < 33250)
             {
-                sss_contrib = 4980.00;
+                sss_contrib = 1650.00;
             }
             else if (gross_income >= 33250 && gross_income < 33750)
             {
-                sss_contrib = 5055.00;
+                sss_contrib = 1675.00;
             }
             else if (gross_income >= 33750 && gross_income < 34250)
             {
-                sss_contrib = 5130.00;
+                sss_contrib = 1700.00;
             }
             else if (gross_income >= 34250 && gross_income < 34750)
             {
-                sss_contrib = 5205.00;
+                sss_contrib = 1725.00;
             }
             else
             {
-                sss_contrib = 5280.00;
+                sss_contrib = 1750.00;
             }
             sss_contrib_txtbox.Text = sss_contrib.ToString("n");
 
@@ -385,21 +388,23 @@ namespace Lesson_3.Exam
             salary_loan = Convert.ToDouble(sal_loan_txtbox.Text);
             other_deduction = Convert.ToDouble(other_loan_txtbox.Text);
 
+            // formulas for calculating loans, contrib, and deduction
             total_loan = sss_loan + pagibig_loan + faculty_sav_loan + faculty_sav_dep + salary_loan + other_deduction;
             total_contrib = sss_contrib + pagibig_contrib + philhealth_contrib + tax_contrib;
             total_deduction = total_contrib + total_loan;
-            total_deduc_txtbox.Text = total_deduction.ToString("n");
+            total_deduc_txtbox.Text = "₱ " + total_deduction.ToString("n");
         }
 
         private void net_income_button_Click(object sender, EventArgs e)
         {
             // formula to compute the desired data to be computed
             netincome = gross_income - total_deduction;
-            net_income_txtbox.Text = netincome.ToString("n");
+            net_income_txtbox.Text = "₱ " + netincome.ToString("n");
         }
 
         private void save_button_Click(object sender, EventArgs e)
         {
+            // call other form to print the payslip
             Prelim_Exam_Prnt print = new Prelim_Exam_Prnt();
             print.employee_code.Text = employee_num_txtbox.Text;
             print.employee_name.Text = first_name_txtbox.Text + " " + middle_name_txtbox.Text + " " + surname_txtbox.Text;
@@ -409,12 +414,15 @@ namespace Lesson_3.Exam
             print.basic_pay.Text = basic_net_income_txtbox.Text;
             print.overtime_pay.Text = other_net_income_txtbox.Text;
             print.hono_pay.Text = hono_net_income_txtbox.Text;
+
             print.tax_deduc.Text = tax_contrib_txtbox.Text;
             print.sss_contrib.Text = sss_contrib_txtbox.Text;
             print.hdmf_contrib.Text = pagibig_contrib_txtbox.Text;
             print.philhealth_contrib.Text = philhealth_contrib_txtbox.Text;
+
             print.total_earnings.Text = gross_income_txtbox.Text;
             print.total_deductions.Text = total_deduc_txtbox.Text;
+
             print.gross_earnings.Text = gross_income_txtbox.Text;
             print.deductions.Text = total_deduc_txtbox.Text;
             print.net_pay.Text = net_income_txtbox.Text;
@@ -423,6 +431,7 @@ namespace Lesson_3.Exam
 
         private void update_btn_Click(object sender, EventArgs e)
         {
+            // remove all user-input textboxes
             basic_numhrs_txtbox.Clear();
             basic_rate_txtbox.Clear();
             hono_numhrs_txtbox.Clear();
@@ -441,6 +450,7 @@ namespace Lesson_3.Exam
 
         private void new_btn_Click(object sender, EventArgs e)
         {
+            // remove all inputted textboxes
             employee_num_txtbox.Clear();
             department_txtbox.Clear();
             designation_txtbox.Clear();
